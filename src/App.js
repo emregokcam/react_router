71acc915c1fced571739fced7bd72eb142fa88e5
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2021-05-19 22:32:26 +0300

    add Router Route Switch Link

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <h1>CLARUSWAY</h1>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <Home />
-      <About />
-      <Contact />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <h1>CLARUSWAY</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h1>This is Contact Page</h1>
+      <h1 style={{ color: "green" }}>This is Contact Page</h1>
     </div>
   );
 };
